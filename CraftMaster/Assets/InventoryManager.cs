f86bf9c1b2def6d6f3ca3512be8e743237d72912
OrdonLukasz <ukaszordon@gmail.com>
2024-08-28 10:43:17 +0200

    Inventory base

---
+++ CraftMaster/Assets/InventoryManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance { get; private set; }
+
+    public List<ItemTypeAndCount> items = new List<ItemTypeAndCount>();
+    [SerializeField] GameObject[] slots = new GameObject[20];
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ CraftMaster/Assets/ItemScriptable.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Scriptable Objects/Item")]
+public class ItemScriptable : ScriptableObject
+{
+    public string name;
+    public Sprite icon;
+    public GameObject prefab;
+    public int stackMax;
+}
